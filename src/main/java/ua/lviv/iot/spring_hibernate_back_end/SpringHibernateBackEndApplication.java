bebfa4eeab0ac239ded32e1c0643438d37caec86
Vitaliy Pashynskyi <vitaliy.pashynskyi@gmail.com>
2022-11-01 12:50:31 +0100

    init Spring + Hibernate project

---
+++ src/main/java/ua/lviv/iot/spring_hibernate_back_end/SpringHibernateBackEndApplication.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.spring_hibernate_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/lviv/iot/spring_hibernate_back_end/SpringHibernateBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.spring_hibernate_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
